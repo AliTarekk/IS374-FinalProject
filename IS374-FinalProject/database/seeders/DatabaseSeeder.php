b4e4470f52a0c2d719b8bc9003341ff382d6b6d2
youssef hatem <youssefhatem8888@gmail.com>
2023-05-22 19:21:51 +0300

    staff => seeder and factory

--- IS374-FinalProject/database/seeders/DatabaseSeeder.php
+++ IS374-FinalProject/database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PersonSeeder::class,
             StudentSeeder::class,
+            StaffSeeder::class,
         ]);
     }
 }
