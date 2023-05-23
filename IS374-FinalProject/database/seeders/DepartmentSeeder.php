f55e10bfb7e5351b326582c4bf6e6de8122162ae
ali.tarek <ali.tarek@robustastudio.com>
2023-05-23 13:47:25 +0300

    Fix DepartmentSeeder

--- IS374-FinalProject/database/seeders/DepartmentSeeder.php
+++ IS374-FinalProject/database/seeders/DepartmentSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\department;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Department;
 
 class DepartmentSeeder extends Seeder
 {
