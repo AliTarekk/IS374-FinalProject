4423e378136450f4f120f6c540d9578b1899ce6e
HassanSalama2001 <elprincemohamed2001@gmail.com>
2023-05-29 19:13:31 +0300

    Modify routes to the new files
    Modify routes to the new files

--- IS374-FinalProject/routes/web.php
+++ IS374-FinalProject/routes/web.php
@@ -27,20 +27,20 @@ use App\Http\Controllers\DepartmentController;
     // })->name("home");
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('layouts.include.dashboard');
 })->name("dashboard");
 
 // Route::get('/dashboard', [StudentController::class, 'index_enroll'])->name("dashboard");
 
 Route::get('/', [HomeController::class, "index"])->name('home');
 
-Route::get('/student_profile', function () {
-    return view('student_profile');
+Route::get('/student/show', function () {
+    return view('layouts.include.student.show');
 })->name("student_profile");
 
-Route::get('/student_course', function () {
-    return view('student_course');
-})->name("student_course");
+// Route::get('/student_course', function () {
+//     return view('student_course');
+// })->name("student_course");
 
 Route::get('/admin', function () {
     return view('layouts.admin');
@@ -49,24 +49,24 @@ Route::get('/admin', function () {
 Route::get('/student/dashboard/{id}', [StudentController::class, 'index_enroll'])->name("student.dashboard");
 
 Route::get('/student/enroll', [StudentController::class, 'create_enroll'])->name("student.enroll");
-Route::get('/student/enroll/index', [StudentController::class, 'index_enroll'])->name("student.index");
+Route::get('/student/enroll/index/{student}', [StudentController::class, 'index_enroll'])->name("student.index");
 Route::get('/student/enroll/add', [StudentController::class, 'add_enroll'])->name("student.add");
 Route::post('/student/enroll/store', [StudentController::class, 'store_enroll'])->name("student.store");
 
-Route::get('/admin_all_course', function () {
-    return view('admin_all_course');
-})->name("admin_all_course");
+// Route::get('/admin_all_course', function () {
+//     return view('admin_all_course');
+// })->name("admin_all_course");
 
-Route::get('/admin_new_course', function () {
-    return view('admin_new_course');
-})->name("admin_new_course");
+// Route::get('/courses/', function () {
+//     return view('layouts.include.courses.create');
+// })->name("admin_new_course");
 
-Route::get('/admin_add_student', function () {
-    return view('admin_add_student');
+Route::get('student/create', function () {
+    return view('layouts.include.admin.student.create');
 })->name("admin_add_student");
 
-Route::get('/admin_details_student', function () {
-    return view('admin_details_student');
+Route::get('student/index', function () {
+    return view('layouts.include.admin.student.index');
 })->name("admin_details_student");
 
 
